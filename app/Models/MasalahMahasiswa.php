409df8424df16f6dc7f9e0cfe357a1ad87290781
septiaulia-afk <septi.aulia@mhs.politala.ac.id>
2025-10-14 09:59:08 +0800

    memperbaiki tampilan berita

--- app/Models/MasalahMahasiswa.php
+++ app/Models/MasalahMahasiswa.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MasalahMahasiswa extends Model
 {
-    //
+    use HasFactory;
+
+    // Nama tabel yang dipakai
+    protected $table = 'masalah_mahasiswa';
+
+    // Kolom yang boleh diisi (fillable)
+    protected $fillable = [
+        'nama',
+        'nim',
+        'prodi',
+        'angkatan',
+        'jenis_permasalahan',
+        'deskripsi',
+        'kategori',
+        'status',
+        'tanggal_laporan',
+        'pelapor',
+    ];
 }
